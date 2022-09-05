fc68b5f6f9cbc38f500bf0e15b736728ec3ff649
Anita Liberatore <anitaliberatore@hotmail.com>
2022-09-05 16:07:37 +0200

    add tailwindcss

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,41 @@
 <template>
-  <div class="home">
-    <h1> ciao</h1>
+  <div class="flex flex-col h-screen max-h-screen">
+    <!-- Search / Results -->
+    <div
+      class="z-20 flex justify-center relative bg-hero-pattern bg-cover px-4 pt-8 pb-32"
+    >
+      <!-- Search Input -->
+      <div class="w-full max-w-screen-sm">
+        <h1 class="text-white text-center text-3xl pb-4">IP Address Tracker</h1>
+        <div class="flex">
+
+
+        </div>
+      </div>
+
+    </div>
+
   </div>
 </template>
 
+<style>
+
+</style>
+
 <script>
+
 
 export default {
   name: 'HomeView',
   components: {
-    
+
+  },
+
+  data() {
+    return {
+      img: './images/pattern-bg.png'
+    }
   }
 }
 </script>
+
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: { content: ["./public/**/*.html", "./src/**/*.vue"] },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      backgroundImage: (theme) => ({
+        "hero-pattern": "url('pattern-bg.png')",
+      }),
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
