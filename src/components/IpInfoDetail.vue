aa6bf851e7cc6f05995dc7a223ffa947b4817f50
Anita Liberatore <anitaliberatore@hotmail.com>
2022-09-09 16:51:34 +0200

    implement IP address detail

---
+++ src/components/IpInfoDetail.vue
@@ -0,0 +1,47 @@
+<template>
+
+    <div
+        class="absolute 
+               -bottom-64 
+               lg:-bottom-20 
+               flex 
+               flex-col 
+               lg:flex-row 
+               bg-white 
+               gap-y-4 
+               gap-x-12 
+               px-8 
+               pt-8 
+               pb-12 
+               rouded-md">
+
+        <div class="flex flex-col">
+            <h3 class="text-xs mb-2 uppercase"> IP Address</h3>
+            <span class="text-2xl">8.8.8.8</span>
+        </div>
+
+        <div class="flex flex-col">
+            <h3 class="text-xs mb-2 uppercase"> Location</h3>
+            <span class="text-2xl">California</span>
+        </div>
+
+        <div class="flex flex-col">
+            <h3 class="text-xs mb-2 uppercase"> Timezone</h3>
+            <span class="text-2xl">UTC-07:00</span>
+        </div>
+
+        <div class="flex flex-col">
+            <h3 class="text-xs mb-2 uppercase"> Isp</h3>
+            <span class="text-2xl">Google Llc</span>
+        </div>
+
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,10 +8,13 @@
       <div class="w-full max-w-screen-sm">
         <h1 class="text-white text-center text-3xl pb-4 text-ip-address">{{textLabelIpAddressTracker}}</h1>
         <div class="flex">
-
-
+          <input class="flex-1 py-3 px-2 rounded-tl-md rounded-bl-md focus:outline-none" type="text" placeholder="Search for any IP address or leave empty to get your ip info">
+          <i class="cursor-pointer bg-black text-white px-4 rounded-tr-md rounded-br-md flex items-center fas fa-chevron-right"></i>
         </div>
       </div>
+
+      <!-- IP info -->
+      <IPInfoDetail/>
 
     </div>
 
@@ -26,12 +29,12 @@
 
 
 <script>
-
+import IPInfoDetail from "../components/IpInfoDetail.vue"
 
 export default {
   name: 'HomeView',
   components: {
-
+    IPInfoDetail
   },
 
   data() {
