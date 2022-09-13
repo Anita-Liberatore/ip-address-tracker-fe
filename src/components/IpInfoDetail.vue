59062193bfc0fb61f1c56feb8121d5dcde330707
Anita Liberatore <anitaliberatore@hotmail.com>
2022-09-13 12:32:42 +0200

    add leaflet working

--- src/components/IpInfoDetail.vue
+++ src/components/IpInfoDetail.vue
@@ -13,25 +13,25 @@
                px-8 
                pt-8 
                pb-12 
-               rouded-md">
+               rounded-md">
 
         <div class="flex flex-col">
-            <h3 class="text-xs mb-2 uppercase"> IP Address</h3>
+            <h3 class="text-xs font-bold mb-2 uppercase"> IP Address</h3>
             <span class="text-2xl">8.8.8.8</span>
         </div>
 
         <div class="flex flex-col">
-            <h3 class="text-xs mb-2 uppercase"> Location</h3>
+            <h3 class="text-xs font-bold mb-2 uppercase"> Location</h3>
             <span class="text-2xl">California</span>
         </div>
 
         <div class="flex flex-col">
-            <h3 class="text-xs mb-2 uppercase"> Timezone</h3>
+            <h3 class="text-xs font-bold mb-2 uppercase"> Timezone</h3>
             <span class="text-2xl">UTC-07:00</span>
         </div>
 
         <div class="flex flex-col">
-            <h3 class="text-xs mb-2 uppercase"> Isp</h3>
+            <h3 class="text-xs font-bold mb-2 uppercase"> Isp</h3>
             <span class="text-2xl">Google Llc</span>
         </div>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,40 +1,68 @@
 <template>
   <div class="flex flex-col h-screen max-h-screen">
     <!-- Search / Results -->
-    <div
-      class="z-20 flex justify-center relative bg-hero-pattern bg-cover px-4 pt-8 pb-32"
-    >
+    <div class="z-20 flex justify-center relative bg-hero-pattern bg-cover px-4 pt-8 pb-32">
       <!-- Search Input -->
       <div class="w-full max-w-screen-sm">
         <h1 class="text-white text-center text-3xl pb-4 text-ip-address">{{textLabelIpAddressTracker}}</h1>
         <div class="flex">
-          <input class="flex-1 py-3 px-2 rounded-tl-md rounded-bl-md focus:outline-none" type="text" placeholder="Search for any IP address or leave empty to get your ip info">
-          <i class="cursor-pointer bg-black text-white px-4 rounded-tr-md rounded-br-md flex items-center fas fa-chevron-right"></i>
+          <input class="flex-1 py-3 px-2 rounded-tl-md rounded-bl-md focus:outline-none" type="text"
+            placeholder="Search for any IP address or leave empty to get your ip info">
+          <i
+            class="cursor-pointer bg-black text-white px-4 rounded-tr-md rounded-br-md flex items-center fas fa-chevron-right"></i>
         </div>
       </div>
 
       <!-- IP info -->
-      <IPInfoDetail/>
+      <IPInfoDetail />
 
     </div>
-
+    <!--Map-->
+    <div id="mapid" class="h-full z-10"></div>
   </div>
 </template>
 
 <style>
-  .text-ip-address {
-    font-weight: 600;
-  }
+.text-ip-address {
+  font-weight: 600;
+}
 </style>
 
 
 <script>
 import IPInfoDetail from "../components/IpInfoDetail.vue"
+import leaflet from "leaflet"
+import { onMounted } from "vue";
 
 export default {
   name: 'HomeView',
   components: {
     IPInfoDetail
+  },
+
+  setup() {
+    let mymap;
+
+    onMounted(() => {
+      mymap = leaflet.map('mapid').setView([51.505, -0.09], 13);
+
+      leaflet
+        .tileLayer(
+          "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYW5pdGFsaWJlcmF0b3JlIiwiYSI6ImNsODAxZ3JlZjAxdGszcnQ5OWMxOGl2aWoifQ.nXYktS1QcKh2aZAOG5e1zg",
+          {
+            attribution:
+              'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: "mapbox/streets-v11",
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken:
+              "pk.eyJ1IjoiYW5pdGFsaWJlcmF0b3JlIiwiYSI6ImNsODAxZ3JlZjAxdGszcnQ5OWMxOGl2aWoifQ.nXYktS1QcKh2aZAOG5e1zg",
+          }
+        )
+        .addTo(mymap);
+    });
+
   },
 
   data() {
